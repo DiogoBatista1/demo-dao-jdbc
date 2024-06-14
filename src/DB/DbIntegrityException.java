3a63831c276013d09285d5a5f26d69f7b18c251a
Diogo Batista <diogobatista330@gmail.com>
2024-06-14 06:07:55 -0300

    Creating project and git repository

---
+++ src/DB/DbIntegrityException.java
@@ -0,0 +1,11 @@
+package DB;
+
+public class DbIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegrityException(String msg) {
+		super(msg);
+	}
+
+}
